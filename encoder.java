ee7126a9df5cfc8e36e17d226384218c98b9bf09
Arslan Ayushin <aayushin40@stuy.edu>
2024-05-23 15:32:14 -0400

    fixed clock function

--- encoder.java
+++ encoder.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class encoder {
     public static void main(String args[]) {
         int[] SESSION_KEY = {0b01001110,0b00101111,0b01001101,0b01111100,0b00011110,0b10111000,0b10001011,0b00111010};
-        long INITIALIZATION_VECTOR = 2000;
+        long INITIALIZATION_VECTOR = 0;
         String Message = "Happy bunnies will live forever.";
         encode(SESSION_KEY,INITIALIZATION_VECTOR,Message);
     }
@@ -31,6 +31,14 @@ public class encoder {
           // }
         }
         // System.out.println("");
+      }
+      for (int i = 0; i >= 0; i--) {
+        LSFR1[Math.floorMod(start1-1,19)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR1[(start1+13)%19] ^ LSFR1[(start1+16)%19] ^ LSFR1[(start1+17)%19] ^ LSFR1[(start1+18)%19];
+        start1 = Math.floorMod(start1-1,19);
+        LSFR2[Math.floorMod(start2-1,22)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR2[(start2+20)%22] ^ LSFR2[(start2+21)%22];
+        start2 = Math.floorMod(start2-1,22);
+        LSFR3[Math.floorMod(start3-1,23)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR3[(start3+7)%23] ^ LSFR3[(start3+20)%23] ^ LSFR3[(start3+21)%23] ^ LSFR3[(start3+22)%23];
+        start3 = Math.floorMod(start3-1,23);
       }
       debugger(LSFR1,start1);
       debugger(LSFR2,start2);
@@ -69,28 +77,32 @@ public class encoder {
       debugger(LSFR3, start3);
       Registers result = new Registers();
       for (int i = 0; i < 100; i++) {
+        boolean clock1 = LSFR1[Math.floorMod(start1 + 8, 19)];
+        boolean clock2 = LSFR2[Math.floorMod(start2 + 10, 22)];
+        boolean clock3 = LSFR3[Math.floorMod(start3 + 10, 23)];
+        System.out.println(clock1);
+        System.out.println(clock2);
+        System.out.println(clock3);
 
+        boolean maj_bit = maj(clock1, clock2, clock3);
+        System.out.println(maj_bit);
+        if (clock1 == maj_bit) {
+          LSFR1[Math.floorMod(start1-1,19)] = LSFR1[(start1+13)%19] ^ LSFR1[(start1+16)%19] ^ LSFR1[(start1+17)%19] ^ LSFR1[(start1+18)%19];
+          start1 = Math.floorMod(start1-1,19);
+        }
+        if (clock2 == maj_bit) {
+          LSFR2[Math.floorMod(start2-1,22)] = LSFR2[(start2+20)%22] ^ LSFR2[(start2+21)%22];
+          start2 = Math.floorMod(start2-1,22);
+        }
+        if (clock3 == maj_bit) {
+          LSFR3[Math.floorMod(start3-1,23)] = LSFR3[(start3+7)%23] ^ LSFR3[(start3+20)%23] ^ LSFR3[(start3+21)%23] ^ LSFR3[(start3+22)%23];
+          start3 = Math.floorMod(start3-1,23);
+        }
+        debugger(LSFR1, start1);
+        debugger(LSFR2, start2);
+        debugger(LSFR3, start3);
       }
-      boolean clock1 = LSFR1[Math.floorMod(start1 + 8, 19)];
-      boolean clock2 = LSFR2[Math.floorMod(start2 + 10, 22)];
-      boolean clock3 = LSFR3[Math.floorMod(start3 + 7, 23)];
-      boolean maj_bit = maj(clock1, clock2, clock3);
-      System.out.println(maj_bit);
-      if (clock1 == maj_bit) {
-        LSFR1[Math.floorMod(start1-1,19)] = LSFR1[(start1+13)%19] ^ LSFR1[(start1+16)%19] ^ LSFR1[(start1+17)%19] ^ LSFR1[(start1+18)%19];
-        start1 = Math.floorMod(start1-1,19);
-      }
-      if (clock2 == maj_bit) {
-        LSFR2[Math.floorMod(start2-1,22)] = LSFR2[(start2+20)%22] ^ LSFR2[(start2+21)%22];
-        start2 = Math.floorMod(start2-1,22);
-      }
-      if (clock3 == maj_bit) {
-        LSFR3[Math.floorMod(start3-1,23)] = LSFR3[(start3+7)%23] ^ LSFR3[(start3+20)%23] ^ LSFR3[(start3+21)%23] ^ LSFR3[(start3+22)%23];
-        start3 = Math.floorMod(start3-1,23);
-      }
-      debugger(LSFR1, start1);
-      debugger(LSFR2, start2);
-      debugger(LSFR3, start3);
+
 
       return result;
     }
