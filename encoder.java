49fbc6a858ac413f1be7bf319f72cb969c8c7cdf
David Cai Liang <dcailiang40@stuy.edu>
2024-05-22 22:00:04 -0400

    Stage 2 breaks after 16 bits.

--- encoder.java
+++ encoder.java
@@ -15,12 +15,22 @@ public class encoder {
       int start1 = 0;
       int start2 = 0;
       int start3 = 0;
-      for (int k = 0; k < 1; k++) {
+      for (int k = 0; k < 3; k++) {
       for (int i = 7; i >= 0; i--) {
         LSFR1[Math.floorMod(start1-1,19)] = ((SESSION_KEY[k] >> i & 1) != 0) ^ LSFR1[(start1+13)%19] ^ LSFR1[(start1+16)%19] ^ LSFR1[(start1+17)%19] ^ LSFR1[(start1+18)%19];
         start1 = Math.floorMod(start1-1,19);
+        LSFR2[Math.floorMod(start2-1,22)] = ((SESSION_KEY[k] >> i & 1) != 0) ^ LSFR2[(start2+20)%22] ^ LSFR2[(start2+21)%22];
+        start2 = Math.floorMod(start2-1,22);
+        LSFR3[Math.floorMod(start3-1,23)] = ((SESSION_KEY[k] >> i & 1) != 0) ^ LSFR3[(start3+7)%23] ^ LSFR3[(start3+20)%23] ^ LSFR3[(start3+21)%23] ^ LSFR3[(start3+22)%23];
+        start3 = Math.floorMod(start3-1,23);
+        if (k==2) {
         debugger(LSFR1,start1);
+        debugger(LSFR2,start2);
+        debugger(LSFR3,start3);
+        System.out.println(" ");
+      }
     }
+    System.out.println("");
     }
       return "";
     }
