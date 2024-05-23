e9ed62e673b637baba63fc56b14606e1d98831ab
Arslan Ayushin <aayushin40@stuy.edu>
2024-05-23 19:52:53 -0400

    steps 1-4 working

--- encoder.java
+++ encoder.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class encoder {
     public static void main(String args[]) {
         int[] SESSION_KEY = {0b01001110,0b00101111,0b01001101,0b01111100,0b00011110,0b10111000,0b10001011,0b00111010};
-        long INITIALIZATION_VECTOR = 0;
+        long INITIALIZATION_VECTOR = 2000;
         String Message = "Happy bunnies will live forever.";
         encode(SESSION_KEY,INITIALIZATION_VECTOR,Message);
     }
@@ -32,14 +32,14 @@ public class encoder {
         }
         // System.out.println("");
       }
-      for (int i = 0; i >= 0; i--) {
-        LSFR1[Math.floorMod(start1-1,19)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR1[(start1+13)%19] ^ LSFR1[(start1+16)%19] ^ LSFR1[(start1+17)%19] ^ LSFR1[(start1+18)%19];
-        start1 = Math.floorMod(start1-1,19);
-        LSFR2[Math.floorMod(start2-1,22)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR2[(start2+20)%22] ^ LSFR2[(start2+21)%22];
-        start2 = Math.floorMod(start2-1,22);
-        LSFR3[Math.floorMod(start3-1,23)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR3[(start3+7)%23] ^ LSFR3[(start3+20)%23] ^ LSFR3[(start3+21)%23] ^ LSFR3[(start3+22)%23];
-        start3 = Math.floorMod(start3-1,23);
-      }
+      // for (int i = 21; i >= 0; i--) {
+      //   LSFR1[Math.floorMod(start1-1,19)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR1[(start1+13)%19] ^ LSFR1[(start1+16)%19] ^ LSFR1[(start1+17)%19] ^ LSFR1[(start1+18)%19];
+      //   start1 = Math.floorMod(start1-1,19);
+      //   LSFR2[Math.floorMod(start2-1,22)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR2[(start2+20)%22] ^ LSFR2[(start2+21)%22];
+      //   start2 = Math.floorMod(start2-1,22);
+      //   LSFR3[Math.floorMod(start3-1,23)] = ((INITIALIZATION_VECTOR >> i & 1) != 0) ^ LSFR3[(start3+7)%23] ^ LSFR3[(start3+20)%23] ^ LSFR3[(start3+21)%23] ^ LSFR3[(start3+22)%23];
+      //   start3 = Math.floorMod(start3-1,23);
+      // }
       debugger(LSFR1,start1);
       debugger(LSFR2,start2);
       debugger(LSFR3,start3);
