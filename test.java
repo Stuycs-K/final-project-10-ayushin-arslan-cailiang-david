7c23fe985184b91d50b6bdfd1d9d63969dc51740
Arslan Ayushin <aayushin40@stuy.edu>
2024-05-31 06:37:36 -0400

    opens pipe as RandomAccessFile

--- test.java
+++ test.java
@@ -2,16 +2,34 @@ import java.io.*;
 
 public class test {
     public static void main(String[] args) {
-        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-        String line = "";
-        while (line != null) {
-            try {
-                line = f.readLine();
-                System.out.println(line);
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        String PipeLocation = "mario";
+        File pipe = new File(PipeLocation);
+        if(!pipe.exists()) { 
+            System.out.println("Named pipe does not exist");
+            System.exit(0);
+        }
+        System.out.println("A");
+        try {
+            RandomAccessFile mario = new RandomAccessFile(PipeLocation, "rw");
+
+            System.out.println("A");
+
+            String line = "";
+            while (line != null) {
+                try {
+                    line = stdin.readLine();
+                    System.out.println(line);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
         }
     }
 
